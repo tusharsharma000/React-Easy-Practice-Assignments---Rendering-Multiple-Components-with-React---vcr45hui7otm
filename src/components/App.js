cd3582140d3743dcdcd1f2b62ee4eb490a2d8d96
tusharsharma000 <84301919+tusharsharma000@users.noreply.github.com>
2022-04-05 18:11:44 +0530

    String

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <>
+      <div id="main">
+        <h1 data-ns-test="project-name">Netflix-Clone</h1>
+        <p data-ns-test="project-description">
+          This is the Description of the project.
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default App;
